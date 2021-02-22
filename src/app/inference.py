b8e3f74546a8ef7b5ea71fe5f321c2c761e26d9e
Andrew Barton <andrew.barton.usyd@gmail.com>
2021-02-22 23:39:46 +1100

    Got inference working with web app textarea input, returning the most likely topic

--- src/app/inference.py
+++ src/app/inference.py
@@ -1,1 +1,14 @@
-# TODO
+import joblib
+import pandas as pd
+
+def predict_topics(text):
+    lda, count_vectorizer, topic_names = load_model()
+    features = count_vectorizer.transform([text])
+    preds = pd.DataFrame(lda.transform(features), columns=topic_names)
+    return preds
+
+def load_model():
+    lda = joblib.load('model/model.joblib')
+    count_vectorizer = joblib.load('model/count_vectorizer.joblib')
+    topic_names = joblib.load('model/topic_names.joblib')
+    return lda, count_vectorizer, topic_names
